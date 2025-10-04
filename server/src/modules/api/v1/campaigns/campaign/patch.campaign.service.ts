932539fa562b3eaea09ef47570d052e7d739fe7e
fabbricca <fabb.riccardo@gmail.com>
2025-10-04 19:09:42 +0200

    fix(server): correct import type

--- server/src/modules/api/v1/campaigns/campaign/patch.campaign.service.ts
+++ server/src/modules/api/v1/campaigns/campaign/patch.campaign.service.ts
@@ -1,5 +1,5 @@
 import type { FastifyRequest } from "fastify";
-import { UpdateCampaign } from "@campaign-service/types";
+import type { UpdateCampaign } from "@campaign-service/types";
 
 export async function patchCampaignService(request: FastifyRequest<{ Params: { campaignId: string } }>, businessIds: string[], input: UpdateCampaign) {
   const { repository } = request.server;
